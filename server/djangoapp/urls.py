c603a95d0da50b3a3004871e971e065d26299a1e
Justin Fiedler <jcf03@hotmail.com>
2023-02-05 13:52:04 -0500

    User Management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,11 +19,15 @@ urlpatterns = [
 
     # path for registration
 
+    path(route='registration', view=views.registration_request, name='registration'),
+
     # path for login
 
     path(route='login', view=views.login_request, name='login'),
 
     # path for logout
+
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
